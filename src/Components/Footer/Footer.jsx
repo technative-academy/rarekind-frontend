11163c243612218eae9dada76a695e718c674a77
Matt Nightingale <matt.snail@gmail.com>
2025-11-09 18:36:58 +0000

    changed footer links to link to our repos

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,21 +1,35 @@
-import styles from "./Footer.module.css";
-import React from "react";
-
+import styles from './Footer.module.css'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
-  return (
-    <div className={styles.wrapper}>
-      <div>
-          <button className={styles.footerLink}>Footer link 1</button>
-          <button className={styles.footerLink}>Footer link 2</button>
-          </div>
-          <div>
-          <button className={styles.footerSubLink}>Terms and Conditions</button>
-          <button className={styles.footerSubLink}>Cookies</button>
-          </div>
-          <p>© 2025 RareKind</p>
-    </div>
-  );
+    return (
+        <div className={styles.wrapper}>
+            <div>
+                <Link
+                    to="https://github.com/technative-academy/rarekind-frontend"
+                    target='blank'
+                    className={styles.footerLink}
+                >
+                    Front-end Github repo
+                </Link>
+                <Link
+                    to="https://github.com/technative-academy/rarekind-backend"
+                    target='blank'
+                    className={styles.footerLink}
+                >
+                    Back-end Github repo
+                </Link>
+            </div>
+            <div>
+                <button className={styles.footerSubLink}>
+                    Terms and Conditions
+                </button>
+                <button className={styles.footerSubLink}>Cookies</button>
+            </div>
+            <p>© 2025 RareKind</p>
+        </div>
+    )
 }
 
-export default Footer;
+export default Footer
