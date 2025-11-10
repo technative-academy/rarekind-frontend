54dff4e16a29a5db5f71c1ea081be5ed9c6c72bd
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-10 12:20:26 +0000

    Styling one of my collections page

--- src/Pages/OneOfMyCollectionsPage/OneOfMyCollectionsPage.jsx
+++ src/Pages/OneOfMyCollectionsPage/OneOfMyCollectionsPage.jsx
@@ -1,20 +1,21 @@
 import styles from './OneOfMyCollectionsPage.module.css'
+import { FaPencil, FaTrashCan } from 'react-icons/fa6'
 
 function OneOfMyCollectionsPage() {
     return (
         <section className={styles.wrapper}>
             <div className={styles.collectionContainer}>
-                <h2>Title of Collection </h2>
+                <h2 className={styles.pageTitle}>Title of Collection </h2>
                 <div className={styles.descriptionContainer}>
-                    <h3>Description</h3>
-                    <p>
+                    <h3 className={styles.title}>Description</h3>
+                    <p className={styles.description}>
                         Additional info filled up by the user when creating
                         collection. This text could be quite long so we don't
                         want it to be in a narrow column.
                     </p>
                 </div>
                 <div className={styles.additionalContainer}>
-                    <h3>Additional</h3>
+                    <h3 className={styles.title}>Additional</h3>
                     <p>
                         Additional info filled up by the user when creating
                         collection. This text could be quite long so we don't
@@ -22,7 +23,7 @@ function OneOfMyCollectionsPage() {
                     </p>
                 </div>
                 <div className={styles.tagsContainer}>
-                    <h3>Tags</h3>
+                    <h3 className={styles.title}>Tags</h3>
                     <ul className={styles.tagList}>
                         <li className={styles.tag}>Tag</li>
                         <li className={styles.tag}>Tag</li>
@@ -43,8 +44,14 @@ function OneOfMyCollectionsPage() {
                     </p>
                 </div>
                 <div>
-                    <button>Edit Collection</button>
-                    <button>Delete Collection</button>
+                    <button className={styles.editCollectionButton}>
+                        <FaPencil className={styles.icon} />
+                        Edit Collection
+                    </button>
+                    <button className={styles.deleteCollectionButton}>
+                        <FaTrashCan className={styles.icon} />
+                        Delete Collection
+                    </button>
                 </div>
             </div>
         </section>
