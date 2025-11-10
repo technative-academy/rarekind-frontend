47adb0a21a0a3bd3ffe1b97f4c57eff0fffda1cf
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-10 14:37:54 +0000

    Creating component for items

---
+++ src/Components/Items/Items.jsx
@@ -0,0 +1,9 @@
+import styles from './Items.module.css'
+
+export default function Items() {
+    return (
+        <section className={styles.wrapper}>
+            <p>Items section</p>
+        </section>
+    )
+}
