8528fbcd62819198fda286bcc8a93ebf4f0cfd29
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-10 15:41:08 +0000

    Styled empty items view

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -1,10 +1,21 @@
 import styles from './Items.module.css'
 
+import { FaInbox, FaPlus } from 'react-icons/fa6'
+
 export default function Items() {
     return (
         <section className={styles.wrapper}>
-            <p>No items added, yet</p>
-            <p>Would you like to add one now?</p>
+            <div className={styles.noItemsContainer}>
+                <FaInbox className={styles.inboxIcon} />
+                <p className={styles.textBold}>No items added, yet</p>
+                <p className={styles.textLight}>
+                    Would you like to add one now?
+                </p>
+                <button className={styles.addItemButton}>
+                    <FaPlus className={styles.addIcon} />
+                    ADD ITEM
+                </button>
+            </div>
         </section>
     )
 }
