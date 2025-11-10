582d1da5048fe4130044821d14c84074760f5261
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-10 14:55:35 +0000

    Items styling

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -3,7 +3,8 @@ import styles from './Items.module.css'
 export default function Items() {
     return (
         <section className={styles.wrapper}>
-            <p>Items section</p>
+            <p>No items added, yet</p>
+            <p>Would you like to add one now?</p>
         </section>
     )
 }
