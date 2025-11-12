fc4c0b7088315e0e3b50d3f391146ee90f26d85d
Matt Nightingale <matt.snail@gmail.com>
2025-11-12 14:23:38 +0000

    added the same logic to mycollections page to prevent error. Can change this once we have the login working

--- src/Pages/MyCollectionsPage/MyCollectionsPage.jsx
+++ src/Pages/MyCollectionsPage/MyCollectionsPage.jsx
@@ -1,19 +1,33 @@
 import styles from './MyCollectionsPage.module.css'
 import CollectionCard from '../../Components/CollectionCard/CollectionCard'
 import CollectionSearch from '../../Components/CollectionSearch/CollectionSearch'
+import { useDispatch, useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
+import { fetchCollections } from '../../API/collectionAPISlice'
 
 export default function MyCollectionsPage() {
+    const dispatch = useDispatch()
+    const collections = useSelector((state) => state.collections.items)
+    const status = useSelector((state) => state.collections.status)
+    const error = useSelector((state) => state.collections.error)
+
+    useEffect(() => {
+        if (status === 'idle') {
+            dispatch(fetchCollections())
+        }
+    }, [dispatch, status])
+
     return (
         <section className={styles.wrapper}>
             <p>All Collections</p>
             <CollectionSearch />
             <div className={styles.grid}>
-                <CollectionCard />
-                <CollectionCard />
-                <CollectionCard />
-                <CollectionCard />
-                <CollectionCard />
-                <CollectionCard />
+                {collections.map((collection) => (
+                    <CollectionCard
+                        key={collection.id}
+                        collection={collection}
+                    />
+                ))}
             </div>
         </section>
     )
